35e55a6b64b47c14deb77d153ed837ec0d9991c4
Nicholas Starke <starke.nicholas@gmail.com>
2016-07-14 21:11:11 -0500

    Adding route for images

--- iccmobile-server/index.js
+++ iccmobile-server/index.js
@@ -1,10 +1,13 @@
 var request = require('request');
 var express = require('express');
 var hbs = require('express-hbs');
+var fs = require('fs');
 
 var app = express();
 
 var port = process.env.PORT || 8080;
+
+var defaultImage = fs.readFileSync('iowacodecamp-mobile/public/images/default.png');
 
 // Use `.hbs` for extensions and find partials in `views/partials`.
 app.engine('hbs', hbs.express4({
@@ -22,6 +25,14 @@ app.get('/', function(req, res) {
       body: body
     });
   });
+});
+
+app.get('/images/:imageId', function(req, res){
+  if (!req.params.imageId || req.params.imageId == 'null') {
+    res.set('Content-type', 'image/png`');
+    return res.end(defaultImage);
+  }
+  request.get('http://iowacodecamp.com/public/images/speakers/' + req.params.imageId).pipe(res);
 });
 
 app.listen(port, function(){
