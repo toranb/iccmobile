3bf675d59e5ef891e60fd4a35c2b89e208208b4f
toran billups <toranb@gmail.com>
2017-07-08 11:45:01 -0500

    bump to ember 2.14.0 stable

--- iowacodecamp-mobile/ember-cli-build.js
+++ iowacodecamp-mobile/ember-cli-build.js
@@ -5,10 +5,10 @@ module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     'esw-index': {
       location: 'index.html',
-      version: '26'
+      version: '27'
     },
     'asset-cache': {
-      version: '26',
+      version: '27',
       include: [
         'assets/vendor.js',
         'assets/iowacodecamp.js',
