f2972ff24ac12abd868a3ae3d8a0a050e9fa6fee
toran billups <toranb@gmail.com>
2018-04-24 06:13:01 -0500

    SERVER: added post route for reviews

--- iccmobile-server/index.js
+++ iccmobile-server/index.js
@@ -1,6 +1,7 @@
 var request = require('request');
 var express = require('express');
 var compression = require('compression');
+var bodyParser = require('body-parser');
 var fs = require('fs');
 
 var app = express();
@@ -10,6 +11,7 @@ var port = process.env.PORT || 8080;
 var defaultImage = fs.readFileSync(__dirname + '/../iowacodecamp-mobile/public/images/default.png');
 
 app.use(compression());
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
 app.get('/api/sessions', function(req, res) {
@@ -25,6 +27,36 @@ app.get('/images/:imageId', function(req, res){
     return res.end(defaultImage);
   }
   request.get('http://iowacodecamp.com/public/images/speakers/' + req.params.imageId).pipe(res);
+});
+
+var ratings = {};
+app.get('/api/reviews', function(req, res) {
+  return res.json(ratings);
+});
+
+app.post('/api/reviews/:uuid/:sessionId', function(req, res){
+  var rating = null;
+  var uuid = req.params.uuid;
+  var sessionId = req.params.sessionId;
+
+  try {
+    rating = parseInt(req.body.rating, 10);
+  } catch(e) {
+    console.log('error: invalid rating value');
+  }
+
+  if (!rating || (!uuid || uuid == 'null') || (!sessionId || sessionId == 'null')) {
+    res.status(400);
+    return res.json({ 'error': 'invalid' });
+  }
+
+  var theRatings = ratings[uuid] || {};
+  if (Object.keys(theRatings).length === 0) {
+    ratings[uuid] = { [sessionId]: rating };
+  }else{
+    ratings[uuid][sessionId] = rating;
+  }
+  return res.json({ 'success': 'valid' });
 });
 
 app.listen(port, function(){
